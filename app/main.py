9ede3dfd909684c3ce27508c8e67ebd98b078ef8
maelghrib <maelghrib@gmail.com>
2025-12-31 14:51:03 +0200

    added and setup fastapi and sqlite database

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from .database import create_db_and_tables
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the AI Agent Platform!"}
