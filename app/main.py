e2fe94f15d853d54d4e6d0ce5472ba158451c63c
maelghrib <maelghrib@gmail.com>
2025-12-31 21:39:52 +0200

    fix lifespan error

--- app/main.py
+++ app/main.py
@@ -3,24 +3,24 @@ from contextlib import asynccontextmanager
 from .database import create_db_and_tables
 from .routers import agents
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
 app = FastAPI(
     title="AI Agent Platform Backend API",
     description="A backend API for a platform that enables users to create, manage, and interact with AI agents via text and voice.",
     version="1.0.0",
     openapi_url="/openapi.json",
     docs_url="/docs",
-    redoc_url="/redoc"
+    redoc_url="/redoc",
+    lifespan=lifespan,
 )
 
-
-@asynccontextmanager
-async def lifespan(fastapi_app: FastAPI):
-    create_db_and_tables()
-    yield
-
-
-app.router.lifespan_context = lifespan(fastapi_app=app)
-app.include_router(agents.router)
+app.include_router(agents.router, prefix="/agents")
 
 
 @app.get("/")
